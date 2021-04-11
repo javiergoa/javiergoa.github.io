5ed1081b9a6ca1e3c9db7629939e6c7bff602fc8
javier <jga0112@ciencias.unam.mx>
2021-04-11 16:22:14 -0700

    first commit

---
+++ _bibliography/auto/papers.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "einstein1956investigations"
+    "einstein1950meaning"
+    "GGK3"
+    "PhysRev.47.777"
+    "einstein1905molekularkinetischen"
+    "einstein1905movement"
+    "einstein1905electrodynamics"))
+ :bibtex)
+
