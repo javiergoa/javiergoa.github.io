4fc152d4756f55c85aec507a2aed0505a69deac3
javier <jga0112@ciencias.unam.mx>
2021-04-11 16:38:52 -0700

    first commit

--- _bibliography/auto/papers.el
+++ _bibliography/auto/papers.el
@@ -5,9 +5,6 @@
     "einstein1956investigations"
     "einstein1950meaning"
     "GGK3"
-    "PhysRev.47.777"
-    "einstein1905molekularkinetischen"
-    "einstein1905movement"
-    "einstein1905electrodynamics"))
+    "PhysRev.47.777"))
  :bibtex)
 
